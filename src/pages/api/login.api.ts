16937868af58f363a138504edbb5914d315faccc
cazma <mcaz.dev@gmail.com>
2023-04-30 09:43:21 +0900

    temp

--- src/pages/api/login.api.ts
+++ src/pages/api/login.api.ts
@@ -1,6 +1,37 @@
 import type { NextApiHandler } from 'next';
 
-import { assignSession, login } from '@/env/backend';
+// import { assignSession, login } from '@/env/backend';
+
+import type { NextApiResponse } from 'next';
+import { setCookie } from 'nookies';
+
+import { authConfig } from '@/config';
+
+export const assignSession = (res: NextApiResponse, idToken: string) => {
+  setCookie({ res }, authConfig.name, idToken, authConfig.cookie);
+};
+
+export const removeSession = (res) => {
+  res.setHeader(
+    'Set-Cookie',
+    `${authConfig.name}=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly`
+  );
+};
+
+export const login = async (email: string, password: string) => {
+  const body = JSON.stringify({ email, password, returnSecureToken: true });
+
+  return await fetch(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${authConfig.key}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body,
+    }
+  );
+};
 
 export const handler: NextApiHandler = async (req, res) => {
   const { email, password } = req.body;
